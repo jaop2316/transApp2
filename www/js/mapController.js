25100906fc77cca29bf15f4642464e634d23f4a6
jaop2316 <jessikita.ldu@gmail.com>
2017-12-14 18:00:06 -0500

    Add sidebar routes in map section

--- www/js/mapController.js
+++ www/js/mapController.js
@@ -1,37 +1,140 @@
 app.controller('mapController',function($scope,$ionicPopup,$ionicSideMenuDelegate){
 
-	var src = 'https://www.dropbox.com/s/0qn1veaz4o5akcz/Rutas-Transplaneta.kml?dl=1';
+// 	var src = 'https://www.dropbox.com/s/tpphzum3bsx1m5l/firstRoute.kml?dl=1';
+//
+//     //var latLng = new google.maps.LatLng(-0.24540368195453613,-78.51656848144529);
+// //https://www.dropbox.com/s/tpphzum3bsx1m5l/firstRoute.kml?dl=0
+// //https://www.dropbox.com/s/0qn1veaz4o5akcz/Rutas-Transplaneta.kml?dl=1
+//     var mapOptions = {
+//       center: {lat:-0.24540368195453613, lng:-78.51656848144529},
+//       zoom: 11,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP,
+//       draggable:true
+//     };
+//
+//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//
+//     loadKmlLayer(src, map);
+//
+//     function loadKmlLayer(src, map) {
+//     	console.log(src);
+//     	console.log(map);
+//     var kmlLayer = new google.maps.KmlLayer(src, {
+//       suppressInfoWindows: true,
+//       preserveViewport: false,
+//       map:map
+//     });
+//     google.maps.event.addListener(kmlLayer, 'click', function(event) {
+//       var content = event.featureData.infoWindowHtml;
+//       console.log(content);
+//        var alertPopup = $ionicPopup.alert({
+//        title: 'Información',
+//        template: content
+//      });
+//     });
+//  }
 
-    //var latLng = new google.maps.LatLng(-0.24540368195453613,-78.51656848144529);
+var layers = [];
 
+layers[0] = new google.maps.KmlLayer('https://www.dropbox.com/s/3uq07wy1ss5bk2n/firstRoute.kml?dl=1', {
+    preserveViewport: true
+});
+
+layers[1] = new google.maps.KmlLayer('https://www.dropbox.com/s/2awzpmw8spz4ym2/SecondRoute.kml?dl=1', {
+    preserveViewport: true
+});
+
+layers[2] = new google.maps.KmlLayer('https://www.dropbox.com/s/vxniqzxtuyh1gpe/thirdRoute.kml?dl=1', {
+    preserveViewport: true
+});
+
+layers[3] = new google.maps.KmlLayer('https://www.dropbox.com/s/m9tn1ytbhdha5u5/fourthRoute.kml?dl=1', {
+    preserveViewport: true
+});
+
+layers[4] = new google.maps.KmlLayer('https://www.dropbox.com/s/0gldyct3gjx3nbf/fifthRoute.kml?dl=1', {
+    preserveViewport: true
+});
+
+layers[5] = new google.maps.KmlLayer('https://www.dropbox.com/s/9sw3erta3zxnex3/sixroute.kml?dl=1', {
+    preserveViewport: true
+});
+
+// end layers to toggle
+// intialize
+
+function initialize() {
     var mapOptions = {
-      center: {lat:-0.24540368195453613, lng:-78.51656848144529},
-      zoom: 11,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      draggable:true
+        zoom: 11,
+        center: {lat:-0.24540368195453613, lng:-78.51656848144529},
+        mapTypeId:google.maps.MapTypeId.ROADMAP
     };
 
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    loadKmlLayer(src, map);
+    loadKml = function (opts, map) {
+        var layer = new google.maps.KmlLayer();
+        opts.preserveViewport = true;
+        if (map) {
+            opts.map = map;
+        }
 
-    function loadKmlLayer(src, map) {
-    	console.log(src);
-    	console.log(map);
-    var kmlLayer = new google.maps.KmlLayer(src, {
-      suppressInfoWindows: true,
-      preserveViewport: false,
-      map:map
+        google.maps.event.addListener(layer, 'defaultviewport_changed', function () {
+            var map = this.getMap(),
+                bounds = map.get('kmlBounds') || this.getDefaultViewport();
+
+            bounds.union(this.getDefaultViewport());
+            map.set('kmlBounds', bounds);
+            map.fitBounds(bounds);
+        });
+        layer.setOptions(opts);
+        return layer;
+    };
+
+    function toggleLayers(i) {
+        if (layers[i].getMap() == null) {
+            layers[i].setMap(map);
+        } else {
+            layers[i].setMap(null);
+        }
+    }
+    // end toggle layers
+    google.maps.event.addDomListener(document.getElementById('layer_01'), 'click', function (evt) {
+        toggleLayers(0);
     });
-    google.maps.event.addListener(kmlLayer, 'click', function(event) {
-      var content = event.featureData.infoWindowHtml;
-      console.log(content);
-       var alertPopup = $ionicPopup.alert({
-       title: 'Información',
-       template: content
-     });
+    google.maps.event.addDomListener(document.getElementById('layer_02'), 'click', function (evt) {
+        toggleLayers(1);
     });
- }
+
+		google.maps.event.addDomListener(document.getElementById('layer_03'), 'click', function (evt) {
+        toggleLayers(2);
+    });
+
+		google.maps.event.addDomListener(document.getElementById('layer_04'), 'click', function (evt) {
+        toggleLayers(3);
+    });
+
+		google.maps.event.addDomListener(document.getElementById('layer_05'), 'click', function (evt) {
+        toggleLayers(4);
+    });
+
+		google.maps.event.addDomListener(document.getElementById('layer_06'), 'click', function (evt) {
+        toggleLayers(5);
+    });
+    // toggle layers at the beginning
+    toggleLayers(0);
+    toggleLayers(1);
+		toggleLayers(2);
+		toggleLayers(3);
+		toggleLayers(4);
+		toggleLayers(5);
+
+
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
  $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
@@ -41,54 +144,7 @@ app.controller('mapController',function($scope,$ionicPopup,$ionicSideMenuDelegat
 
 
 
- /*map = L.map('map', {
-    dragging:true
-}).setView([-0.24540368195453613,-78.51656848144529], 11);
 
-
-	     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamExOTc5IiwiYSI6ImNpazcyZHFtcjAxOGJ2ZGt0NGNhamQ1cXQifQ.Kkz4bJY_fOE6PM9YaWzJIg',
-	                    {
-	            attribution: 'Map data &copy; <a href="http://openo streetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	            id: 'mapbox.streets'
-	        }).addTo(map);
-
-	     var circle = L.circle([-0.24540368195453613,-78.51656848144529],500,{
-	         color: 'red',
-	         fillColor: '#f03',
-	         fillOpacity: 0.5,
-	         radius: 500
-	     }).addTo(map);
-
-
-	     var kmlLayer = new L.KML("lib/Rutas-Transplaneta.kml", {async: true});
-        console.log(kmlLayer);
-	    kmlLayer.on("loaded", function(e) {
-            map.fitBounds(e.target.getBounds());
-         });
-         map.addLayer(kmlLayer);*/
-
-
-   /*$scope.map = {
-          defaults: {
-            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
-            zoomControlPosition: 'bottomleft'
-          },
-          markers : {},
-          events: {
-            map: {
-              enable: ['context'],
-              logic: 'emit'
-            }
-          }
-        };
-
-        $scope.map.center  = {
-          lat : -0.24540368195453613,
-          lng : -78.51656848144529,
-          zoom : 12
-        };
-*/
 
 
 	});
